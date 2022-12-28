3f24d7835d0a25bfad8ba44787973f15b426bd83
Bereket Zelalem <64479153+bereket-09@users.noreply.github.com>
2022-12-28 22:28:18 +0300

    fixed meetup details backend

--- components/meetups/meetupDetail.tsx
+++ components/meetups/meetupDetail.tsx
@@ -1,14 +1,15 @@
-import { Fragment } from "react";
 import classes from './meetupDetail.module.css'
-function MeetupDetail(props:any){
+function MeetupDetails(props){
+  console.log("🚀 ~ file: meetupDetail.tsx:3 ~ MeetupDetail ~ props", props)
+  
     return (
         <section className={classes.detail}>
-          <img src={props.image} alt="image" />
-          <h1>{props.title}</h1>
-          <address>{props.address}</address>
-          <p>{props.description}</p>
+          <img src={props?.image} alt="image" />
+          <h1>{props?.title}</h1>
+          <address>{props?.address}</address>
+          <p>{props?.description}</p>
         </section>
       );
 }
 
-export default MeetupDetail
+export default MeetupDetails
--- pages/[meetUpId]/index.tsx
+++ pages/[meetUpId]/index.tsx
@@ -1,19 +1,22 @@
-import MeetupDetail from "../../components/meetups/meetupDetail";
-import { MongoClient ,ObjectId} from "mongodb";
+import MeetupDetails from "../../components/meetups/meetupDetail";
+import { MongoClient, ObjectId } from "mongodb";
 
-function MeetupDetailsPage(props:any) {
+function MeetupDetailsPage(props: any) {
+  console.log(
+    "🚀 ~ file: index.tsx:5 ~ MeetupDetailsPage ~ props",
+    props.meetupData
+  );
   return (
-    <MeetupDetail
-      image={props.image}
-      title={props.title}
-      address={props.address}
-      description={props.description}
+    <MeetupDetails
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 }
 
 export async function getStaticPaths() {
-
   const client = await MongoClient.connect(
     "mongodb+srv://nextjs-2022:nextjs2022@cluster0.bmmne.mongodb.net/?retryWrites=true&w=majority"
   );
@@ -21,30 +24,27 @@ export async function getStaticPaths() {
 
   const meetupsCollection = db.collection("meetups");
 
-  const meetups = await meetupsCollection.find({}, {_ids: 1}).toArray();
+  const meetups = await meetupsCollection.find({}, { _ids: 1 }).toArray();
 
-  console.log("🚀 TEST", meetups);
+  // console.log("🚀 TEST", meetups);
 
   client.close();
 
-
   return {
     fallback: false,
-    paths: meetups.map(meetup=>({
+    paths: meetups.map((meetup) => ({
       params: {
         meetUpId: meetup._id.toString(),
       },
-    }))
-    
+    })),
   };
 }
 
 export async function getStaticProps(context: any) {
   //fetch data for a single meetup
 
-  
   const meetupID = context.params.meetUpId;
-  
+
   const client = await MongoClient.connect(
     "mongodb+srv://nextjs-2022:nextjs2022@cluster0.bmmne.mongodb.net/?retryWrites=true&w=majority"
   );
@@ -52,17 +52,24 @@ export async function getStaticProps(context: any) {
 
   const meetupsCollection = db.collection("meetups");
 
-  const selectedMeetup=await meetupsCollection.findOne({_id: ObjectId(meetupID)})
-  console.log("🚀 TEST", selectedMeetup);
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: ObjectId(meetupID),
+  });
+  // console.log("🚀 TEST", selectedMeetup);
 
   client.close();
 
-
-  console.log("🚀 ~ file: index.tsx:19 ~ getStaticProps ~ meetupId", meetupID);
+  // console.log("🚀 ~ file: index.tsx:19 ~ getStaticProps ~ meetupId", meetupID);
 
   return {
     props: {
-      meetupData: selectedMeetup
+      meetupData: {
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.data.title,
+        address: selectedMeetup.data.address,
+        description: selectedMeetup.data.description,
+        image: selectedMeetup.data.image,
+      },
     },
   };
 }
